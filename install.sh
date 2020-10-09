57e41aa350dcaa746dff56135d2d7ebc231687da
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-09 22:43:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,11 +19,36 @@ sleep 0.5
 clear
 sleep 0.5
 
-echo -e "Thanks for using xdbars, enjoy the feeing of being a true soydev with you're epic tiling wm rice and CLI file manager, we hope it feel the void in your heart."
+echo -e "Thanks for using xdbars, enjoy the feeing of being a true soydev with you're epic tiling wm rice and CLI file manager, we hope it fills the void in your heart."
 
 sleep 0.5
 
 #install needed packages
-echo -e "This will install all needed packages (st, bspwm, sxhkd, ranger, polybar and rofi) to provide you with a fully functional yet minimal system. Without these installed deploy.sh won't work properly and then running bloatdestroyer.sh will give you a broken system (this is bad), that is why we highly recommend running this. Remember, you can cncel at any time with "ctrl + c"
+echo -e "This will install all needed packages (st, bspwm, sxhkd, ranger, polybar and rofi) to provide you with a fully functional yet minimal system. Without these installed deploy.sh won't work properly and then running bloatdestroyer.sh will give you a broken system (this is bad), that is why we highly recommend running this. Remember, you can cncel at any time with ctrl + c [yes] [no]"
 
-bash 
+read option1
+
+case $option1 in
+   yes)
+      bash setup.sh
+   ;;
+   
+   no)
+      echo -e "We highly recommmend not running any other script except the hardening one as they may result in a non-fucntional system. However if you know wwhat you are doing, go ahead and run them."
+   ;;
+esac
+
+echo "Now on to the bloat destroyer script. This will remove all things replaced by newly installed packages, this is optional as some users may want to keep their previous DE/WM, file manager and terminal emulator. [yes] [no]
+
+read option2
+
+case $option2 in
+   yes)
+      bash bloatdestroyer.sh
+    ;;
+   
+   no)
+      echo "All your old software will remain, run this at any time to remove it automatically."
+    ;;
+esac
+
