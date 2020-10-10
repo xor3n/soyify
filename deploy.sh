525d156bb77aff3c6c320359026e0b9ad695e8a6
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-10 12:06:38 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,7 @@
 # this is demo of deploy script not yet done
 
 # cp -r files/dotfiles/themes/tomorrow-night/* files/config/
-echo -e "enter your user, this is to make sure configuration occurs properly"
-read user
-clear
-cp -r files/dotfiles/themes/tomorrow-night/* /home/$user/.config/
+cp -r files/dotfiles/themes/tomorrow-night/* /home/$USER/.config/
 #echo "are u sure?"
 #echo "sike no going back"
 
