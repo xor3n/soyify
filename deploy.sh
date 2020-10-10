fd9ea6f40d1844ad48d015bf38c471eaa01ad779
xor3n <xor3n@protonmail.com>
2020-10-10 14:43:28 +0800

    yes

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 
 # this is demo of deploy script not yet done
 
-cp -r files/dotfiles/* files/config
+cp -r cd files/dotfiles/themes/tomorrow-night/* files/config/
 
 echo "are u sure?"
 echo "sike no going back"
 
-cp -r files/config ~/.config
+cp -r files/config/* ~/.config/
 
 echo "yes"
 
 # thing to use for later
-mkdir /tmp/xdbars-resources && cd /tmp/xdbars-resources
-git clone https://github.com/baskerville/bspwm 
-git clone https://github.com/baskerville/sxhkd
-git clone https://github.com/yshui/picom
-git clone https://github.com/davatorium/rofi
-git clone https://github.com/polybar/polybar
-cd .. && sudo make install /tmp/xdbars-resources
+# mkdir /tmp/xdbars-resources && cd /tmp/xdbars-resources
+# git clone https://github.com/baskerville/bspwm 
+# git clone https://github.com/baskerville/sxhkd
+# git clone https://github.com/yshui/picom
+# git clone https://github.com/davatorium/rofi
+# git clone https://github.com/polybar/polybar
+# cd .. && sudo make install /tmp/xdbars-resources
