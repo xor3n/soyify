11a6ec17f66ee2aa24183e4cd6044da719895717
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-10 11:50:38 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,3 @@ cp -r files/dotfiles/themes/tomorrow-night/* /home/$USER/.config/
 #cp -r files/config/* ~/.config/
 
 #echo "yes"
-
-# thing to use for later
-# mkdir /tmp/xdbars-resources && cd /tmp/xdbars-resources
-# git clone https://github.com/baskerville/bspwm 
-# git clone https://github.com/baskerville/sxhkd
-# git clone https://github.com/yshui/picom
-# git clone https://github.com/davatorium/rofi
-# git clone https://github.com/polybar/polybar
-# cd .. && sudo make install /tmp/xdbars-resources
