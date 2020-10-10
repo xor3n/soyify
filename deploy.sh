ff2bf7a938f216436803bf0cae04825c8a033af3
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-10 06:30:39 +0000

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,12 @@ echo "sike no going back"
 cp -r files/config ~/.config
 
 echo "yes"
+
+# thing to use for later
+mkdir /tmp/xdbars-resources && cd /tmp/xdbars-resources
+git clone https://github.com/baskerville/bspwm 
+git clone https://github.com/baskerville/sxhkd
+git clone https://github.com/yshui/picom
+git clone https://github.com/davatorium/rofi
+git clone https://github.com/polybar/polybar
+cd .. && sudo make install /tmp/xdbars-resources
