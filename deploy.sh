bc44326d157fe6ff91d89446ac0c9f96d4c4b7f1
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-10 12:24:57 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-# deploys the dotfiles
-
-# this is demo of deploy script not yet done
-
+# deploys dotfiles
 # cp -r files/dotfiles/themes/tomorrow-night/* files/config/
 cp -r files/dotfiles/themes/tomorrow-night/* /home/$USER/.config/
-#echo "are u sure?"
-#echo "sike no going back"
-
 #cp -r files/config/* ~/.config/
-
-#echo "yes"
