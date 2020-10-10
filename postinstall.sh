b7e2a302803caf65c15930884baef989af885815
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-10 12:31:33 +0000

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# yes
-echo "need root"
-
 G="\033[1;34m[*] \033[0m"
 S="\033[1;32m[+] \033[0m"
 E="\033[1;31m[-] \033[0m"
@@ -17,12 +14,14 @@ sleep 0.5
 clear
 sleep 0.5
 
-echo -e "Welcome to postinstall.sh, this will clear all bloat and do any necessary configuration. This is optional, if you wish to keep your old desktop environment, terminal emulator and file manager, feel free to ctrl + c and enjoy your newly set up environment."
-
-sleep 0.2
+echo -e "Welcome to postinstall.sh, this will clear all bloat and do any necessary configuration. This is optional, if you wish to keep your old desktop environment, terminal emulator and file manager, feel free to ctrl + c and enjoy your newly set up environment. Enter [start] to begin."
+read start
+if [ $start == start]
+then
+	clear
+fi
 
 echo -e "Enter [yes] or [no] to continue"
-
 read choice
 
 case $choice in
