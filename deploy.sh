d3e0fc40c1617e615c67a4e7ee1f91432a80b73d
cubeperson <72589674+cubeperson@users.noreply.github.com>
2020-10-11 00:44:44 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+
+POSIXLY_CORRECT=1
+
 # deploys dotfiles
 # cp -r files/dotfiles/themes/tomorrow-night/* files/config/
 cp -r files/dotfiles/themes/tomorrow-night/* /home/$USER/.config/
