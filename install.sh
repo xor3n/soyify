b2d0c7ae7d5c11ae3b91cf583244b69ed0f1b1c8
xor3n <xor3n@github.com>
2021-02-07 01:49:05 +0800

    Updated install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# install bspwm sxhkd picom polybar and other packages
+# TODO: emerge and apt
+
+#doas emerge x11-wm/bspwm x11-misc/polybar x11-misc/sxhkd etc...
+#sudo apt install bspwm polybar sxhkd etc...
+
